509cbe1225bd660f22d5dc69c132ef0eca36552f
Esteban <esteban.sastre@tenforce.com>
2017-08-25 10:09:21 +0200

    Added queries

--- test/test-suite.sh
+++ test/test-suite.sh
@@ -20,21 +20,21 @@ while IFS=  read -r query
 do
     echo -e "${PURPLE}Query: ${YELLOW}${query} to Nginx${NC}" 
     nginx_start=$(date +%s)
-    for i in {1..10}
+    for i in {1..5}
     do
         curl -s -w '\n' -XPOST -H 'Content-Type: application/x-www-form-urlencoded' --data-urlencode query="$query" $nginx_url > /dev/null
     done
     nginx_stop=$(date +%s)   
-    nginxavg=$(echo "scale=4; ($nginx_stop - $nginx_start) / 10" | bc -l)
+    nginxavg=$(echo "scale=4; ($nginx_stop - $nginx_start) / 5" | bc -l)
     
     echo -e "${PURPLE}Query: ${YELLOW}${query} to Virtuoso${NC}"
     virtuoso_start=$(date +%s)
-    for i in {1..10}
+    for i in {1..5}
     do
         curl -s -w '\n' -XPOST -H 'Content-Type: application/x-www-form-urlencoded' --data-urlencode query="$query" $virtuoso_url > /dev/null
     done
     virtuoso_stop=$(date +%s)   
-    virtuosoavg=$(echo "scale=4; ($virtuoso_stop - $virtuoso_start) / 10" | bc -l)
+    virtuosoavg=$(echo "scale=4; ($virtuoso_stop - $virtuoso_start) / 5" | bc -l)
 
     decrease=$(echo "scale=4; $virtuosoavg - $nginxavg" | bc -l)
     perc_decrease=$(echo "scale=4; $decrease / $virtuosoavg * 100" | bc -l)   
